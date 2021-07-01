2ef793dbd395a2bd1331bfff03d37d98a4a4e8f8
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-07-01 17:36:05 +0530

    fix: title function issue

--- src/views/layout.lisp
+++ src/views/layout.lisp
@@ -6,7 +6,7 @@
      (:html :lang "en"
 	    (:head
 	     (:meta :charset "utf-8")
-	     (:title, title)
+	     (:title ,title)
 	     (:link :href "/styles.css" :rel "stylesheet")
 	     )
 	    (:body 
