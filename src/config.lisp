08acb3eac511c7ee8a8ad07b62d69a74345819f8
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-07-02 12:39:17 +0530

    fix: unmatched ( in config file

--- src/config.lisp
+++ src/config.lisp
@@ -3,7 +3,7 @@
 (setf (html-mode) :html5)
 
 ;; (add-template-directory (asdf:system-relative-pathname "hello-world" "templates/"))
-(add-template-directory  "/app/templates/"))
+(add-template-directory  "/app/templates/")
 
 (defparameter +base.html+ (djula:compile-template* "base.html"))
 
