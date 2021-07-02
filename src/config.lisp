3e51cef8df081b3b529a4c5cb11b06e98e78d356
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-07-02 12:09:23 +0530

    fix: templates directory issue

--- src/config.lisp
+++ src/config.lisp
@@ -2,7 +2,8 @@
 
 (setf (html-mode) :html5)
 
-(add-template-directory (asdf:system-relative-pathname "hello-world" "templates/"))
+;; (add-template-directory (asdf:system-relative-pathname "hello-world" "templates/"))
+(add-template-directory (merge-pathnames #P"templates/" (asdf:system-source-directory "hello-world" )))
 
 (defparameter +base.html+ (djula:compile-template* "base.html"))
 
